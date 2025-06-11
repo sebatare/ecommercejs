a2b130f9b7535cce88ced8e16a7130bc58091427
sebatare <sebatapiareb@gmail.com>
2025-06-11 09:45:11 -0400

    c

--- backend/src/infrastructure/db/pool.js
+++ backend/src/infrastructure/db/pool.js
@@ -2,7 +2,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
+    user: 'sebastian',
     host: 'localhost',
     database: 'reactecommerce',
     password: 'admin',
