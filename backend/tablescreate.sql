9e5c720810664c8b635d3f9de9b0147a111a4934
sebatare <sebatapiareb@gmail.com>
2025-08-17 15:36:06 -0400

    sql

--- backend/tablescreate.sql
+++ backend/tablescreate.sql
@@ -1,10 +1,12 @@
---RELACIONES UNO A MUCHO
-CREATE TABLE roles (
+--RELACIONES UNO A MUCHOS
+CREATE TABLE roles
+(
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE users (
+CREATE TABLE users
+(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -13,28 +15,27 @@ CREATE TABLE users (
     role_id INT REFERENCES roles(id)
 );
 
-
-
-CREATE TABLE carts (
+CREATE TABLE carts
+(
     id SERIAL PRIMARY KEY,
-    user_id INT UNIQUE REFERENCES users(id), -- <--- UNIQUE aquí
+    user_id INT UNIQUE REFERENCES users(id),
     status VARCHAR(20) DEFAULT 'open',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-CREATE TABLE orders (
+CREATE TABLE orders
+(
     id SERIAL PRIMARY KEY,
     user_id INT REFERENCES users(id),
     status VARCHAR(20) DEFAULT 'pending',
     total NUMERIC(10, 2) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
---RELACIONES MUCHO A MUCHO
+--RELACIONES MUCHOS A MUCHOS
 
-CREATE TABLE products (
+CREATE TABLE products
+(
     id SERIAL PRIMARY KEY,
     title VARCHAR(100),
     description TEXT,
@@ -43,15 +44,16 @@ CREATE TABLE products (
     image_url TEXT
 );
 
-CREATE TABLE cart_items (
+CREATE TABLE cart_items
+(
     cart_id INT REFERENCES carts(id),
     product_id INT REFERENCES products(id),
     quantity INT DEFAULT 1,
     PRIMARY KEY (cart_id, product_id)
 );
 
-
-CREATE TABLE order_items (
+CREATE TABLE order_items
+(
     order_id INT REFERENCES orders(id),
     product_id INT REFERENCES products(id),
     quantity INT,
@@ -59,23 +61,22 @@ CREATE TABLE order_items (
     PRIMARY KEY (order_id, product_id)
 );
 
-CREATE TABLE wishlist (
+CREATE TABLE wishlist
+(
     user_id INT REFERENCES users(id),
     product_id INT REFERENCES products(id),
     PRIMARY KEY (user_id, product_id)
 );
 
-
-CREATE TABLE categories (
+CREATE TABLE categories
+(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE product_categories (
+CREATE TABLE product_categories
+(
     product_id INT REFERENCES products(id),
     category_id INT REFERENCES categories(id),
     PRIMARY KEY (product_id, category_id)
 );
-
-
-
