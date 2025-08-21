73a618c7b5fb26c818a5425092c8dfde32127687
sebatare <sebatapiareb@gmail.com>
2025-08-21 16:12:16 -0400

    cambios de puertos y url api

--- frontend/src/utils/axios.ts
+++ frontend/src/utils/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: 'http://localhost:3002/ecommerce/api',
 })
 
 api.interceptors.request.use((config) => {
