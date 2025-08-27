bc61ce217ce09a9bd32ee3acc6a79e050896e718
sebatare <sebatapiareb@gmail.com>
2025-08-27 13:37:12 -0400

    he sedido

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -11,7 +11,7 @@ const routes = [
   { path: '/login', component: LoginPage },
   { path: '/cart', component: CartPage },
   { path:'/register', component: RegisterPage },
-  { path: '/admin', component: HomeAdminPage },
+  { path: '/administrador', component: HomeAdminPage },
   { path: '/admin/products', component: ProductsAdminPage },
 ]
 
